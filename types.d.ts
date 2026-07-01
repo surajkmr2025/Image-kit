ab422139fa06eff9ba4c6f0e26d567fc2213aea4
Suraj Kumar <surajkumar44727@gmail.com>
2026-07-01 11:07:14 +0530

    chore: add global types for mongoose and next-auth

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,31 @@
 import { Connection } from "mongoose"
 
+//Mongoose global connection cache
 declare global {
     var mongoose: {
         conn: Connection | null
         promise: Promise<Connection> | null
-    }
+    };
+}
+// 2. NextAuth Session and User Extensions
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+    provider?: string;
+  }
+}
+
+// 3. NextAuth JWT Extension
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
 }
 
 export {}
