b14d821aa24d2345a6f0064eaa3c3d4c611abb38
Suraj Kumar <surajkumar44727@gmail.com>
2026-07-03 04:54:44 +0530

    chore: add providers and fix hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster, toast } from "sonner";
+import Providers from "./components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}
+      <body className="min-h-full flex flex-col">
+        <Providers>
+          {children}
+        </Providers>
         <Toaster richColors position="top-right" />
       </body>
     </html>
